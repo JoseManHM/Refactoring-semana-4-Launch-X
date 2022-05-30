22ab1f97e28300eecefb8aa7be4989b8cffe9f38
José Manuel Hernández Mayorga <manuelmayorga99@gmail.com>
2022-05-30 08:17:19 -0500

    Server with express done, and run perfectly

---
+++ app/lib/server.js
@@ -0,0 +1,34 @@
+const ExplorerController = require("./controllers/ExplorerController");
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.json({message: "FizzBuzz Api Welcome"});
+});
+
+app.get("/v1/explorers/:mission", (req, res) => {
+    //Envia los explorers en dicha mission
+    const mission = req.params.mission;
+    const explorersInMission = ExplorerController.getExplorersByMission(mission);
+    res.json(explorersInMission);
+});
+
+app.get("/v1/explorers/amount/:mission", (req, res) => {
+    //Envia la mision y la cantidad de explorers
+    const mission = req.params.mission;
+    const amountExplorersInMission = ExplorerController.getExplorersAmountByMission(mission);
+    res.json({mission: req.params.mission, quantity: amountExplorersInMission});
+});
+
+app.get("/v1/explorers/usernames/:mission", (req, res) => {
+    //Envia los nombres de usuario de los explorers de una mission
+    const mission = req.params.mission;
+    const usernamesByMission = ExplorerController.getExplorersUsernamesByMission(mission);
+    res.json({mission: mission, explorers: usernamesByMission});
+});
+
+app.listen(port, () => {
+    console.log(`Fizzbuzz API in localhost: ${port}`);
+});
